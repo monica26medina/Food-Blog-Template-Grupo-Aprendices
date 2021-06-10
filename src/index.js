8a88decd5e55e52181c82adb4e99bb387f00d192
monica26 <medinaquiaberth@gmail.com>
2021-06-10 00:04:34 -0400

    inicio de cartas

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const container = document.getElementById("app");
+const element =<App />;
+
+ReactDOM.render(element, container); 
